2b2ef4329e7b0bc6354d6504edf8efb185a35167
rileyanderson <5150r.d.anderson@gmail.com>
2016-04-11 17:35:50 -0600

    Movie Search
    Initial Commit

---
+++ MovieSearch/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  MovieSearch
+//
+//  Created by Riley Anderson on 4/11/16.
+//  Copyright © 2016 Riley Anderson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Movie
+{
+    
+}
