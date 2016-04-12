79cee3c630803f3bc1643de221161bbd463b2b54
rileyanderson <5150r.d.anderson@gmail.com>
2016-04-12 11:57:04 -0600

    Started working on ListViewController
    Need to add images next

--- MovieSearch/MovieListViewController.swift
+++ MovieSearch/MovieListViewController.swift
@@ -8,19 +8,26 @@
 
 import UIKit
 
-class MovieListViewController: UIViewController, UITextFieldDelegate
+class MovieListViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource
 {
     //private var searches = [SearchResults]()
     
     var movie: SearchResults = SearchResults()
     var myMovies:SearchResults?
     var arr = Array<Movie>()
-     let sampleTextField = UITextField(frame: CGRectMake(0, 0, 200, 20))
-
+    let sampleTextField = UITextField(frame: CGRectMake(0, 0, 200, 20))
+    var tableView = UITableView(frame: UIScreen.mainScreen().bounds, style: UITableViewStyle.Plain)
+    override func viewDidAppear(animated: Bool) {
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad()
     {
-        
-       
+        self.view.backgroundColor = UIColor.whiteColor()
+        tableView.delegate      =   self
+        tableView.dataSource    =   self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.view.addSubview(self.tableView)
         sampleTextField.placeholder = "Enter text here"
         sampleTextField.font = UIFont.systemFontOfSize(15)
         sampleTextField.borderStyle = UITextBorderStyle.RoundedRect
@@ -31,55 +38,65 @@ class MovieListViewController: UIViewController, UITextFieldDelegate
         sampleTextField.contentVerticalAlignment = UIControlContentVerticalAlignment.Center
         sampleTextField.delegate = self
         navigationItem.titleView = sampleTextField
-        self.view.backgroundColor = UIColor.whiteColor()
-     
-            
-           
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        }
-        
-        
-        
-//        myMovies = SearchResults()
-//        myMovies!.delegate = self
-//        self.myMovies!.delegate = self
-        
-        
-        
-        
-        
-      
-        
-    
-
-func textFieldShouldReturn(textField: UITextField) -> Bool
-{
-    movie.getMovieData(sampleTextField.text!){responseObject in
-        
-        
-        for a in responseObject
-        {
-            print(a.title)
-        }
         
     }
     
-  return true
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return arr.count
+    }
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
+        
+        let movie:Movie = arr[indexPath.row]
+        
+        cell.textLabel!.text = movie.title
+        cell.detailTextLabel!.text = movie.description
+        
+        //        var myImage: UIImage!
+        //        dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+        //
+        //            myImage =  UIImage(data: NSData(contentsOfURL: NSURL(string:"https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2.png")!)!)
+        //        })
+        //
+        //        cell.imageView!.image = myImage
+        //        var cache = ImageLoadingWithCache()
+        //        var imageView:UIImageView = UIImageView()
+        //        cache.getImage("https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2.png", imageView: imageView, defaultImage: "Poster")
+        //
+        //        cell.imageView!.image = imageView.image
+        
+        return cell
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        movie.getMovieData(sampleTextField.text!){responseObject in
+            
+            
+            //            for a in responseObject
+            //            {
+            //                print(a.title)
+            //            }
+            self.arr = responseObject
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.tableView.reloadData()
+            })
+            
+        }
+        
+        
+        return true
+        
     }
     
 }
